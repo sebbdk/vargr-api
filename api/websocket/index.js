7e04cea1795f77961b26dd6f20ccb9c17a41b453
Kasper Sebb' brandt <sebb@sebb.dk>
2019-05-01 23:42:24 +0200

    Add basic socket methods and tests

--- api/websocket/index.js
+++ api/websocket/index.js
@@ -1,71 +1,91 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const websockify = require('koa-websocket');
-const { lowdb } = require('vargr-dbai');
 
-const router = new Router();
-const app = websockify(new Koa());
-
-function addSubscriptions(sock, collectionName, subscriptions) {
-    if (!subscriptions[collectionName]) {
-        subscriptions[collectionName] = [];
-    }
-
-    subscriptions[collectionName].push(sock);
-}
-
-function removeSubscriptions(sock, collectionName, subscriptions) {
-    if (!subscriptions[collectionName]) {
-        subscriptions[collectionName] = [];
-    }
-
-    subscriptions[collectionName] = subscriptions[collectionName].filter((s) => s != sock);
-}
 
 function dispatchMessage(msg, collectionName, subscriptions, ignore = []) {
     if (subscriptions[collectionName]) {
         subscriptions[collectionName].forEach((sock) => {
             if (ignore && ignore.indexOf(sock) === -1) {
-                sock.send(msg);
+                sock.send(JSON.stringify(msg));
             }
         });
     }
 }
 
-// CRUD
-function listAction(collectionName, sock, message) {}
-function viewAction(collectionName, sock, message) {}
-function addAction(collectionName, sock, message) {}
-function updateAction(collectionName, sock, message) {}
-function deleteAction(collectionName, sock, message) {}
-function pingAction(collectionName, sock, message) {}
+function handleDisconnect(collectionName, dbi, sock, message) {}
 
-// Subscribtions
-function subViewAction(collectionName, sock, message) {}
-function unSubViewAction(collectionName, sock, message) {}
+const actions = {
+    addAction: async (collectionName, dbi, sock, req, subs) => {
+        const resData = await dbi.create(collectionName, req);
+        dispatchMessage({
+            action: req.action,
+            data: resData
+        }, collectionName, subs);
+    },
+    updateAction: async (collectionName, dbi, sock, req, subs) => {
+        const method = Array.isArray(req.data) ? 'updateMany' : 'updateOne';
+        const resData = await dbi[method](collectionName, req);
+        dispatchMessage({
+            action: req.action,
+            data: resData
+        }, collectionName, subs);
+    },
+    deleteAction: async (collectionName, dbi, sock, req, subs) => {
+        const resData = await dbi.delete(collectionName, req);
+        dispatchMessage({
+            action: req.action,
+            data: {
+                where: req.where
+            }
+        }, collectionName, subs);
+    },
+    pingAction: async (collectionName, dbi, sock, req, subs) => {
+        sock.send(JSON.stringify({
+            action: req.action,
+            data: {
+                servertime: performance.now(),
+                clienttime: req.clienttime
+            }
+        }));
+    },
+    subAction: (collectionName, dbi, sock, message, subs) => {
+        if (!subs[collectionName]) {
+            subs[collectionName] = [];
+        }
+        subs[collectionName].push(sock);
+    },
+    unsubAction: (collectionName, dbi, sock, message, subs) => {
+        if (!subs[collectionName]) {
+            subs[collectionName] = [];
+        }
+        subs[collectionName] = subs[collectionName].filter((s) => s != sock);
+    }
+}
 
-function subListAction(collectionName, sock, message) {}
-function unSubListAction(collectionName, sock, message) {}
+module.exports =  function sock(dbi) {
+    const subscriptions = {};
+    const router = new Router();
+    const app = websockify(new Koa());
 
-function handleDisconnect(collectionName, sock, message) {}
+    router.get(`/:collectionName`, ctx => {
+        const collectionName = ctx.params.collectionName;
 
-const subscriptions = {};
-router.get(`/:collectionName`, ctx => {
-    const collectionName = ctx.params.collectionName;
+        actions.subAction(collectionName, dbi, ctx.websocket, null, subscriptions)
 
-    ctx.websocket.on('message', (message) => {
-        message.
-        dispatchMessage(message, collectionName, subscriptions)
+        ctx.websocket.on('message', (rawReq) => {
+            const requests = rawReq[0] === "[" ? JSON.parse(rawReq) : [JSON.parse(rawReq)];
+
+            requests.forEach((req) => {
+                actions[req.action + 'Action'](collectionName, dbi, ctx.websocket, req, subscriptions);
+            });
+        });
+
+        ctx.websocket.on('close', () => {
+            actions.unsubAction(collectionName, dbi, ctx.websocket, null, subscriptions)
+        });
     });
 
-    addSubscriptions(ctx.websocket, collectionName, subscriptions)
-
-    ctx.websocket.on('close', () => {
-        removeSubscriptions(ctx.websocket, collectionName, subscriptions);
-    });
-});
-
-app.ws.use(router.routes())
-
-
-app.listen(3000);
+    app.ws.use(router.routes());
+    return app;
+}
--- api/websocket/index.test.js
+++ api/websocket/index.test.js
@@ -1,8 +1,93 @@
+const sock = require('./index.js');
+const { lowdb } = require('vargr-dbai');
+
 describe(`Test API methods`, () => {
+    let ws = null;
+    let dbi = null;
+    let sockRef = null;
+
+    beforeAll(async () => {
+        dbi = new lowdb()
+        await dbi.init()
+        await dbi.createCollection('messages')
+        sockRef = sock(dbi);
+        sockRef.listen(3000);
+
+        return new Promise((done) => {
+            ws = new WebSocket('ws://localhost:3000/messages');
+            ws.onopen = () => {
+                done();
+            };
+        });
+    });
+
+    it('Can add a document', async () => {
+        return new Promise((done) => {
+            ws.onmessage = ({ data }) => {
+                const msg = JSON.parse(data);
+                expect(msg.data.message).toEqual('hello world');
+                done();
+            };
+
+            ws.send(JSON.stringify({
+                action: 'add',
+                data: { message: 'hello world' }
+            }));
+        });
+    });
+
+    it('Can update a document', async () => {
+        await dbi.create('messages', { data: { id: 3, type: 'A', name: 'poppa doe' } });
+
+        return new Promise((done) => {
+            ws.onmessage = ({ data }) => {
+                const msg = JSON.parse(data);
+                expect(msg.data.message).toEqual('hello world');
+                done();
+            };
+
+            ws.send(JSON.stringify({
+                action: 'update',
+                data: { message: 'hello world' },
+                where: { id: 3 }
+            }));
+        });
+    });
 
 
-    it('placeholder', async () => {
-        expect(1).toEqual(1);
+    it('Can delete a document', async () => {
+        await dbi.create('messages', { data: { id: 5, type: 'A', name: 'poppa doe' } });
+
+        return new Promise((done) => {
+            ws.onmessage = ({ data }) => {
+                const msg = JSON.parse(data);
+                expect(msg.data.where.id).toEqual(5);
+                done();
+            };
+
+            ws.send(JSON.stringify({
+                action: 'delete',
+                where: { id: 5 }
+            }));
+        });
+    });
+
+    it('Can ping for servertime', async () => {
+        const mytime = performance.now();
+
+        return new Promise((done) => {
+            ws.onmessage = ({ data }) => {
+                const msg = JSON.parse(data);
+                expect(msg.data.clienttime).toEqual(mytime);
+                expect(msg.data).toHaveProperty('servertime');
+                done();
+            };
+
+            ws.send(JSON.stringify({
+                action: 'ping',
+                clienttime: mytime
+            }));
+        });
     });
 
 });
